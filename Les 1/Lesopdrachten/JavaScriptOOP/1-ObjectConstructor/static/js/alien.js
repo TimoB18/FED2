4becc32ee9ce5b1b98dd87f38070df9d78bc14bb
TimoB18 <Timo@MacBook-Pro-van-Timo-Blom.local>
2013-09-06 15:09:54 +0200

    Adding bob

--- Les 1/Lesopdrachten/JavaScriptOOP/1-ObjectConstructor/static/js/alien.js
+++ Les 1/Lesopdrachten/JavaScriptOOP/1-ObjectConstructor/static/js/alien.js
@@ -20,3 +20,15 @@ var roberta = new Alien('Roberta Rock');
 
 
 rob.speak();
+
+function Person(name) {
+	this.name = name;
+
+	this.speak = function () {
+		console.log('Hi, my name is ' + this.name);
+	}
+}
+
+var bob = new Person('Bob');
+
+bob.speak();
